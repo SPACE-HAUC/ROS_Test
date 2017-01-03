96b7afb3ba521fe66d7a19913db1f6e02b0b235d
david <david_baumann@student.uml.edu>
2017-01-03 14:06:33 -0500

    created repository

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/david/ros_workspace/src
+# Build directory: /home/david/ros_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(my_ros_package)
---
+++ build/my_ros_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/david/ros_workspace/src/my_ros_package
+# Build directory: /home/david/ros_workspace/build/my_ros_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
